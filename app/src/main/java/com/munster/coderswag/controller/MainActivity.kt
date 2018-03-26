0330a0abed26135daf17777f673a8a3d7a863c52
Munster999 <20864076+Munster999@users.noreply.github.com>
2018-03-26 12:29:59 +0100

    Lesson-02 - We will be using the data in Lesson-01 and present it in a 'Listview' - Create a simple listview to our activity - Create our OWN custom layout for our listview cell (i.e. do not use 'simple_list_item_1' but 'category_list_item')

--- app/src/main/java/com/munster/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/munster/coderswag/controller/MainActivity.kt
@@ -2,12 +2,45 @@ package com.munster.coderswag.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.munster.coderswag.R
+import com.munster.coderswag.model.Category
+import com.munster.coderswag.services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category> // Initialise the adapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter( this, // context
+                                android.R.layout.simple_list_item_1, // type of view
+                                DataService.categories) // data that will be required to be adapted
+        categoryListview.adapter = adapter // Now we need to tell the listview WHO it needs to listen to
     }
 }
+
+/*
+LISTVIEW
+---------
+
+- We have a listview, we have data so now we need a way for the data to be inserted into the listview
+- To do this we need certain components:
+
+        DATASOURCE      ----->  ADAPTER         ---->   LISTVIEW
+        (Data Model)            (Interface)             (ListView)
+
+    - An adapter (The 'interface' or 'go-between' the data and the listview)
+    - This takes the data from the datasource, converts the data, formats it and outputs it into the listview component.
+
+- So in order to create an 'ArrayAdapter' we need to do the following things:
+    1 - Initialise the adapter
+    2 - Pass the following params into the adapter:
+        1 - Context
+        2 - The type of view that the 'listview' requires (android.R.layout.simple_list_item_1)
+        3 - the data that will be required to be adapted. (Dataservice.categories)
+    3 - Now we need to tell the listview WHO it needs to listen to (.adapter)
+*/
--- app/src/main/java/com/munster/coderswag/model/Category.kt
+++ app/src/main/java/com/munster/coderswag/model/Category.kt
@@ -1,4 +1,10 @@
 package com.munster.coderswag.model
 
 
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String) {
+
+    override fun toString(): String {
+        return title
+    } // returns the title of the category (NOT the default value)
+
+}
