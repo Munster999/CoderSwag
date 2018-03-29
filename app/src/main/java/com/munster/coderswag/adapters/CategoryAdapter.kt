1f27d83db74a7a41a4d977a1f6ee485b9774e150
Munster999 <20864076+Munster999@users.noreply.github.com>
2018-03-29 13:20:50 +0100

    Lesson-04 CUSTOM LIST VIEW (with an adapter we create ourselves 'REFACTORED VERSION' using VIEWHOLDER pattern) • We have a dataset that we need to present it in a 'listview' • We create our 'listview',  a custom 'list_item_view' and an adapter (CategoryAdapter) • Within the 'CategoryAdapter', we pass into it: 	• Context 	• Categories • We implement the 4 methods (as shown above) • We add in the 'Viewholder' class • The majority of the work is done in the getView() method 	• Here is where we tell the listview what are views will look like 	• We will REUSE our item views here 	• We action a NULL CHECK to confirm if the 'convertView' = null we will inflate it 	• We get the ID references of the Images and names of the views 	• If They have been ALREADY inflated we will reuse them 	• Finally we set our category Image and name • We still have the remaining methods below getItem(), getItemId() & getCount() • Also the ViewHolder class.

--- app/src/main/java/com/munster/coderswag/adapters/CategoryAdapter.kt
+++ app/src/main/java/com/munster/coderswag/adapters/CategoryAdapter.kt
@@ -12,40 +12,54 @@ import com.munster.coderswag.model.Category
 import kotlinx.android.synthetic.main.category_list_item.*
 
 // BaseAdapter() is the adapter that other adapters inherit from. CategoryAdapter requires the 2 params below:
-class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter() {
+class CategoryAdapter(val context: Context, val categories: List<Category>) : BaseAdapter() {
 
-    val context = context
-    val categories = categories
-    // An 'adapter' that inherits from 'BaseAdapter' requires the 4 methods below
+    //val context = context
+    //val categories = categories
+    // An 'adapter' that inherits from 'BaseAdapter' requires the 4 methods below (plus Viewholder)
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
 
-        val categoryView: View // initialise our 'View' output param
-        // we now have a 'categoryView'
-        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
-        // we also have both Image and Name values
-        val categoryImage : ImageView = categoryView.findViewById(R.id.categoryImage)
-        val categoryName : TextView = categoryView.findViewById(R.id.categoryName)
-        println("Heavy Computing") // NOTE: Shows that this way is much more processor intensive
+        val categoryView: View // initialise our 'View' type param
+        val holder: ViewHolder // initialise our 'ViewHolder' type param
+        if (convertView == null) { // Means this is the very first time the views are being presented, Hence, we
+                                   // continue with finding the categoryview and name and then inflating
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+            holder = ViewHolder()
+            holder.categoryImage = categoryView.findViewById(R.id.categoryImage) // reassign using the 'holder' parent object
+            holder.categoryName = categoryView.findViewById(R.id.categoryName) // holder now has references to the IDs
+            // BELOW: Allows us to recycle our 'views' into the holders. (Means we do not have to recreate and re-inflate our views each time)
+            categoryView.tag = holder // This gives a unique value to the CategoryView
+        } else { // if there is already a cell created we will reuse it
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+        }
         val category = categories[position]
         // convert the image name to a resource Id
         val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
-        categoryImage.setImageResource(resourceId)
-        println(resourceId)
-        categoryName.text = category.title
+        holder.categoryImage?.setImageResource(resourceId)
+        holder.categoryName?.text = category.title
         return categoryView
     } // Gets a View that displays the data at the specified position in the data set.
 
     override fun getItem(position: Int): Any {
         return categories[position]
-    } // Gets the data item associated with the specified position in the data set. (i.e. within the ListView)
+    } // Gets the data item associated with the specified position in the data set.
+    // (i.e. within the ListView)
 
     override fun getItemId(position: Int): Long {
         return 0
-    } // Gets the row id associated with the specified position in the list. (i.e. Assigns an Id to each row - Not using in this e.g.)
+    } // Gets the row id associated with the specified position in the list.
+    // (i.e. Assigns an Id to each row - Not using in this e.g.)
 
     override fun getCount(): Int {
         return categories.count()
-    } // How many items are in the data set represented by this Adapter.(i.e. how many categories are in the categories array)
+    } // How many items are in the data set represented by this Adapter.(i.e. how many categories
+    // are in the categories array)
+
+    private class ViewHolder {
+        var categoryImage: ImageView? = null // need this to start out null
+        var categoryName: TextView? = null
+    } // This class holds a reference to the 'categoryImage' & its 'name'
 }
 
 /*
