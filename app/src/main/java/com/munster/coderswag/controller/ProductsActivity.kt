4e86ed2f3851b5b434580a0813e970b47b8777bd
Munster999 <20864076+Munster999@users.noreply.github.com>
2018-04-02 12:19:45 +0100

    Lesson-08 - RecyclerView adapter for PRODUCTS - Add in a 'gridLayoutManager' for both 'portrait' & 'landscape' orientations on 'phone' & 'tablet'

--- app/src/main/java/com/munster/coderswag/controller/ProductsActivity.kt
+++ app/src/main/java/com/munster/coderswag/controller/ProductsActivity.kt
@@ -1,18 +1,41 @@
 package com.munster.coderswag.controller
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
 import com.munster.coderswag.R
+import com.munster.coderswag.adapters.ProductsAdapter
+import com.munster.coderswag.model.Product
+import com.munster.coderswag.services.DataService
+import kotlinx.android.synthetic.main.activity_products.*
 import utilities.EXTRA_CATEGORY
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter : ProductsAdapter  // created outside onCreate() so we can use it elsewhere
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY) // this gets us the category that was clicked on
-        println(categoryType)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType)) // gets initialised here
 
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 3
+        } // This checks if phone is in landscape mode then it creates 3 columns for display
+
+        val screenSize = resources.configuration.screenWidthDp
+        if(screenSize > 720){
+            spanCount = 3
+        } // This checks if tablet then create 3 columns in both orientations
+
+        val layoutManager = GridLayoutManager(this, spanCount) // span count shows the number of columns you require
+        productListView.layoutManager = layoutManager
+        productListView.adapter = adapter
     }
 }
